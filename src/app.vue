15687abda63e5192ccedebd51dfab032afa0ee79
M Sufyan Khan <msufyankhan709@gmail.com>
2021-10-06 10:22:50 +0500

    First commit

---
+++ src/app.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <HeaderBar />
+    <div class="main-section columns"> 
+      <NavBar />
+      <main class="column">
+       <router-view></router-view>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderBar from '@/components/header-bar';
+import NavBar from '@/components/navbar';
+
+export default {
+  name: 'App',
+  components: { HeaderBar,NavBar },
+};
+</script>
+
+<style lang="scss">
+@import '@/design/index.scss';
+</style>
