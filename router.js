5f4e805bdf7cda3c959acae7da171c7c8c8809be
M.Sufyan Khan <67526983+msufyankhan709@users.noreply.github.com>
2021-10-06 11:00:30 +0500

    Add files via upload

---
+++ router.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import PageNotFound from './views/page-not-found'
+
+Vue.use(Router)
+const parseProps=r=>({id:parseInt(r.params.id)});
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      redirect:'/heroes'
+    },
+    {
+      path: '/heroes',
+      name: 'heroes',
+      component: () => import(/* webpackChunkName: "bundle-heroes" */ './views/heroes.vue'),
+    },
+    {
+      path:'/heroes/:id',
+      name:'hero-detail',
+      component: () => import(/* webpackChunkName: "bundle-heroes" */ './views/hero-detail.vue'),
+      props:parseProps,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/about.vue')
+    },
+    {
+      path:'*',
+      component:PageNotFound
+    }
+  ]
+})
